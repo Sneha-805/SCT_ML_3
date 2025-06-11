8d15a70a7507e86c2f81516445ae62bd1e5731b1
Sneha-805 <muddasneha123@gmail.com>
2025-06-11 12:53:45 +0530

    fix the render

--- app.py
+++ app.py
@@ -1,34 +1,7 @@
-from flask import Flask,render_template,request
-import cv2
-import joblib
-import numpy as np
-import os
+from flask import Flask, render_template
 
-app=Flask(__name__)
-UPLOAD_FOLDER="static/uploads"
-app.config['UPLOAD_FOLDER']=UPLOAD_FOLDER
+app = Flask(__name__)
 
-model=joblib.load("svm_model.pkl")
-joblib.dump(model, "svm_model.pkl", compress=3)
-CATEGORIES=["Cat","Dog"]
-
-@app.route('/',methods=['GET','POST'])
-def index():
-    prediction=None
-    filename=None
-    if request.method=='POST':
-        file=request.files['image']
-        if file:
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-            file.save(filepath)
-            img=cv2.imread(filepath,cv2.IMREAD_GRAYSCALE)
-            img=cv2.reshape(img,(100,100)).flatten().reshape(1,-1)
-            result=model.predict(img)
-            prediction=CATEGORIES[result[0]]
-            filename=file.filename
-    return render_template('index.html', prediction=prediction, filename=filename)
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
-
+@app.route('/')
+def home():
+    return "Hello from Cat-Dog Classifier!"
