a6725279a8b3c286479e77867afe52d9c0f795a2
Sneha-805 <muddasneha123@gmail.com>
2025-06-11 13:25:41 +0530

    fixing attribute error in app.py

--- app.py
+++ app.py
@@ -13,21 +13,25 @@ model=joblib.load("svm_model.pkl")
 joblib.dump(model, "svm_model.pkl", compress=3)
 CATEGORIES=["Cat","Dog"]
 
-@app.route('/',methods=['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    prediction=None
-    filename=None
-    if request.method=='POST':
-        file=request.files['image']
+    prediction = None
+    filename = None
+    if request.method == 'POST':
+        file = request.files['image']
         if file:
             filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
             file.save(filepath)
-            img=cv2.imread(filepath,cv2.IMREAD_GRAYSCALE)
-            img=cv2.reshape(img,(100,100)).flatten().reshape(1,-1)
-            result=model.predict(img)
-            prediction=CATEGORIES[result[0]]
-            filename=file.filename
+
+            img = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
+            img = cv2.resize(img, (100, 100))       
+            img = img.reshape(1, -1)                
+
+            result = model.predict(img)
+            prediction = CATEGORIES[result[0]]
+            filename = file.filename
     return render_template('index.html', prediction=prediction, filename=filename)
+
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
